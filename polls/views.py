8d0369531b362f2dc83adbaf8566e14de587e100
Martin Kroll <mrtnkrll@gmail.com>
2021-01-29 18:12:24 +0300

    Changed Choice and Vote views

--- polls/views.py
+++ polls/views.py
@@ -32,9 +32,21 @@ class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
 
 
 class ChoiceList(generics.ListCreateAPIView):
-    queryset = Choice.objects.all()
+    def get_queryset(self):
+        queryset = Choice.objects.filter(question_id=self.kwargs["pk"])
+        return queryset
     serializer_class = ChoiceSerializer
 
 
-class CreateVote(generics.CreateAPIView):
+class CreateVote(APIView):
     serializer_class = VoteSerializer
+
+    def post(self, request, pk, choice_pk):
+        voted_by = request.data.get("voted_by")
+        data = {'choice': choice_pk, 'question': pk, 'voted_by': voted_by}
+        serializer = VoteSerializer(data=data)
+        if serializer.is_valid():
+            vote = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
