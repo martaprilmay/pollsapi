ded676e6918d5d847ca177d80065421ab07ff737
Martin Kroll <mrtnkrll@gmail.com>
2021-01-29 21:00:08 +0300

    Registered new models

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Poll, Question, Choice, Vote
+from .models import Poll, Question, Choice, Vote, Votes, Answer
 
 
 admin.site.register(Poll)
 admin.site.register(Question)
 admin.site.register(Choice)
 admin.site.register(Vote)
+admin.site.register(Votes)
+admin.site.register(Answer)
