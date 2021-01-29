304ec42a3784db079ac23eedaf490ff6dd8eb6f1
Martin Kroll <mrtnkrll@gmail.com>
2021-01-29 18:00:00 +0300

    Changed Vote model (added corresponding question)

--- polls/models.py
+++ polls/models.py
@@ -30,9 +30,9 @@ class Choice(models.Model):
 
 class Vote(models.Model):
     choice = models.ForeignKey(Choice, related_name='votes', on_delete=models.CASCADE)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, null=True, blank=True, on_delete=models.CASCADE)
     poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
     voted_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
-        unique_together = ('poll', 'voted_by')
+        unique_together =('poll', 'question', 'voted_by')
