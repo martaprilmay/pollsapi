bc345ab7798d1820589bb37f10f39b23787577aa
Martin Kroll <mrtnkrll@gmail.com>
2021-01-29 18:25:32 +0300

    Namespacing

--- polls/urls.py
+++ polls/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from .views import PollList, PollDetail, QuestionList, QuestionDetail, ChoiceList, CreateVote
 
+
+app_name = 'polls'
+
 urlpatterns = [
     path('polls/', PollList.as_view(), name="polls_list"),
     path('polls/<int:pk>/', PollDetail.as_view(), name='polls_detail'),
     path('polls/<int:pk>/questions/', QuestionList.as_view(), name="questions_list"),
     path('polls/<int:pk>/questions/<int:q_pk>/', QuestionDetail.as_view(), name='questions_detail'),
-    path('polls/<int:pk>/questions/<int:q_pk>/choices/', ChoiceList.as_view(), name='choice_list'),
+    path('polls/<int:pk>/questions/<int:q_pk>/choices/', ChoiceList.as_view(), name='choices_list'),
     path('polls/<int:pk>/questions/<int:q_pk>/choices/<int:c_pk>/vote/', CreateVote.as_view(), name='create_vote')
 ]
