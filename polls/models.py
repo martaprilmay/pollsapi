d033eb166c83ac8a51aa08059c02d373cd759a0b
Martin Kroll <mrtnkrll@gmail.com>
2021-01-29 21:01:43 +0300

    Question and answer types added

--- polls/models.py
+++ polls/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 
 class Poll(models.Model):
-    poll_name = models.CharField(max_length=100)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    pub_date = models.DateTimeField(auto_now=True)
+    poll_name = models.CharField(max_length=255)
+    start_date = models.DateField(auto_now=True)
+    end_date = models.DateField(null=True)
+    description = models.TextField(null=True)
 
     def __str__(self):
         return self.poll_name
@@ -13,16 +15,21 @@ class Poll(models.Model):
 
 class Question(models.Model):
     poll = models.ForeignKey(Poll, related_name='questions', on_delete=models.CASCADE)
-    question_text = models.CharField(max_length=100)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=255)
+    TYPE = (
+        (1, 'Text'),
+        (2, 'Select'),
+        (3, 'Choice'),
+    )
+    question_type = models.IntegerField(choices=TYPE, default=1)
 
     def __str__(self):
-        return self.question_text
+        return f'{self.question_text} ({self.question_type})'
 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, related_name='choices', null=True, blank=True, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=100)
+    choice_text = models.CharField(max_length=64)
 
     def __str__(self):
         return self.choice_text
@@ -35,4 +42,24 @@ class Vote(models.Model):
     voted_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
-        unique_together =('poll', 'question', 'voted_by')
+        unique_together = ('poll', 'question', 'voted_by')
+
+
+class Votes(models.Model):
+    choices = models.ManyToManyField(Choice, related_name="selected_choices")
+    question = models.ForeignKey(Question, null=True, blank=True, on_delete=models.CASCADE)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    selected_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('poll', 'question', 'selected_by')
+
+
+class Answer(models.Model):
+    answer_text = models.CharField(max_length=128, null=True, blank=True)
+    question = models.ForeignKey(Question, null=True, blank=True, on_delete=models.CASCADE)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    answered_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('poll', 'question', 'answered_by')
--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,12 @@
+from datetime import date
+
 from rest_framework import generics, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
 from .models import Poll, Question, Choice
-from .serializers import PollSerializer, QuestionSerializer, ChoiceSerializer, VoteSerializer
+from .serializers import PollSerializer, QuestionSerializer, ChoiceSerializer, VoteSerializer, VotesSerializer, AnswerSerializer
 
 
 class PollList(APIView):
@@ -12,6 +14,13 @@ class PollList(APIView):
         polls = Poll.objects.all()[:20]
         data = PollSerializer(polls, many=True).data
         return Response(data)
+
+
+class ActivePollsList(generics.ListCreateAPIView):
+    def get_queryset(self):
+        queryset = Poll.objects.filter(start_date__lte=date.today()).filter(end_date__gte=date.today())
+        return queryset
+    serializer_class = PollSerializer
 
 
 class PollDetail(APIView):
@@ -29,7 +38,9 @@ class QuestionList(generics.ListCreateAPIView):
 
 
 class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Question.objects.all()
+    def get_queryset(self):
+        queryset = Question.objects.filter(id=self.kwargs["q_pk"])
+        return queryset
     serializer_class = QuestionSerializer
 
 
@@ -47,6 +58,36 @@ class CreateVote(APIView):
         voted_by = request.data.get("voted_by")
         data = {'choice': c_pk, 'question': q_pk, 'poll': pk, 'voted_by': voted_by}
         serializer = VoteSerializer(data=data)
+        if serializer.is_valid():
+            vote = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CreateVotes(APIView):
+    serializer_class = VotesSerializer
+
+    def post(self, request, pk, q_pk):
+        selected_by = request.data.get("selected_by")
+        choices = request.data.get("choices")
+        data = {'question': q_pk, 'poll': pk, 'choices': choices, 'selected_by': selected_by}
+        serializer = VotesSerializer(data=data)
+        if serializer.is_valid():
+            vote = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CreateAnswer(APIView):
+    serializer_class = AnswerSerializer
+
+    def post(self, request, pk, q_pk):
+        answered_by = request.data.get("answered_by")
+        answer_text = request.data.get("answer_text")
+        data = {'question': q_pk, 'poll': pk, 'answer_text': answer_text, 'answered_by': answered_by}
+        serializer = AnswerSerializer(data=data)
         if serializer.is_valid():
             vote = serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
