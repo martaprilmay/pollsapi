2054669450619c9d0a996b5cf28bc0e83a58c982
Martin Kroll <mrtnkrll@gmail.com>
2021-01-29 17:48:49 +0300

    Created Choice and Vote routes

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import PollList, PollDetail, QuestionList, QuestionDetail
+from .views import PollList, PollDetail, QuestionList, QuestionDetail, ChoiceList, CreateVote
 
 urlpatterns = [
     path('polls/', PollList.as_view(), name="polls_list"),
     path('polls/<int:pk>', PollDetail.as_view(), name='polls_detail'),
     path('questions/', QuestionList.as_view(), name="questions_list"),
     path('questions/<int:pk>', QuestionDetail.as_view(), name='questions_detail'),
+    path('choices/', ChoiceList.as_view(), name='choice_list'),
+    path('vote', CreateVote.as_view(), name='create_vote')
 ]
