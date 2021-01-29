7c94883c6519e203fef3a10eac11aa427db465ab
Martin Kroll <mrtnkrll@gmail.com>
2021-01-29 18:13:59 +0300

    Changed Choice and Vote routes

--- polls/urls.py
+++ polls/urls.py
@@ -3,9 +3,9 @@ from .views import PollList, PollDetail, QuestionList, QuestionDetail, ChoiceLis
 
 urlpatterns = [
     path('polls/', PollList.as_view(), name="polls_list"),
-    path('polls/<int:pk>', PollDetail.as_view(), name='polls_detail'),
+    path('polls/<int:pk>/', PollDetail.as_view(), name='polls_detail'),
     path('questions/', QuestionList.as_view(), name="questions_list"),
-    path('questions/<int:pk>', QuestionDetail.as_view(), name='questions_detail'),
-    path('choices/', ChoiceList.as_view(), name='choice_list'),
-    path('vote/', CreateVote.as_view(), name='create_vote')
+    path('questions/<int:pk>/', QuestionDetail.as_view(), name='questions_detail'),
+    path('questions/<int:pk>/choices/', ChoiceList.as_view(), name='choice_list'),
+    path('questions/<int:pk>/choices/<int:choice_pk>vote/', CreateVote.as_view(), name='create_vote')
 ]
