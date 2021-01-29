0992c795fdfab97d5aefdd6fcfb1cd9bb949f8b9
Martin Kroll <mrtnkrll@gmail.com>
2021-01-29 18:15:39 +0300

    Typo

--- polls/urls.py
+++ polls/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('questions/', QuestionList.as_view(), name="questions_list"),
     path('questions/<int:pk>/', QuestionDetail.as_view(), name='questions_detail'),
     path('questions/<int:pk>/choices/', ChoiceList.as_view(), name='choice_list'),
-    path('questions/<int:pk>/choices/<int:choice_pk>vote/', CreateVote.as_view(), name='create_vote')
+    path('questions/<int:pk>/choices/<int:choice_pk>/vote/', CreateVote.as_view(), name='create_vote')
 ]
