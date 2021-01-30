56e761b58feec6f3c2055a38762f2a21cc9fd1c9
Martin Kroll <mrtnkrll@gmail.com>
2021-01-30 20:16:44 +0300

    Fixed AnswerSerializer selected_options problem

--- polls/serializers.py
+++ polls/serializers.py
@@ -5,6 +5,19 @@ from rest_framework import serializers
 from .models import Poll, Question, Choice, Answer, AuthID
 
 
+class AnswerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Answer
+        fields = '__all__'
+        extra_kwargs = {'selected_options': {'required': False, 'allow_null': True}}
+        validators = []
+
+    def to_representation(self, instance):
+        result = super(AnswerSerializer, self).to_representation(instance)
+        return OrderedDict([(key, result[key]) for key in result if result[key] is not None])
+
+
 class ChoiceSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -32,18 +45,6 @@ class PollListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Poll
         fields = ('id', 'poll_name', 'description')
-
-
-class AnswerSerializer(serializers.ModelSerializer):
-    selected_options = ChoiceSerializer(many=True, read_only=True, required=False)
-
-    class Meta:
-        model = Answer
-        fields = '__all__'
-
-    def to_representation(self, instance):
-        result = super(AnswerSerializer, self).to_representation(instance)
-        return OrderedDict([(key, result[key]) for key in result if result[key] is not None])
 
 
 class AuthIDSerializer(serializers.ModelSerializer):
