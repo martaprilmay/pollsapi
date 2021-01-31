5fcf8ecb9b61eb43019e18ddd68226009159242f
Martin Kroll <mrtnkrll@gmail.com>
2021-01-31 14:29:08 +0300

    Typo fix

--- polls/urls.py
+++ polls/urls.py
@@ -9,23 +9,23 @@ from .views import (PollList, ActivePollsList, PollCreate, PollUpdate, PollDetai
 app_name = 'polls'
 
 urlpatterns = [
-    path('polls/', ActivePollsList.as_view(), name="active_polls_list"),
-    path('polls/all', PollList.as_view(), name="polls_list"),
-    path('polls/create', PollCreate.as_view(), name="polls_create"),
-    path('polls/<int:pk>/', PollDetail.as_view(), name='polls_detail'),
-    path('polls/<int:pk>/update', PollUpdate.as_view(), name='polls_update'),
+    path('polls/', ActivePollsList.as_view(), name="active-polls-list"),
+    path('polls/all', PollList.as_view(), name="polls-list"),
+    path('polls/create', PollCreate.as_view(), name="poll-create"),
+    path('polls/<int:pk>/', PollDetail.as_view(), name='polls-detail'),
+    path('polls/<int:pk>/update', PollUpdate.as_view(), name='polls-update'),
 
-    path('polls/<int:pk>/questions/', QuestionList.as_view(), name="questions_list"),
-    path('polls/<int:pk>/questions/create', QuestionCreate.as_view(), name="questions_create"),
-    path('polls/<int:pk>/questions/<int:q_pk>/', QuestionDetail.as_view(), name='questions_detail'),
-    path('questions/<int:pk>/update', QuestionUpdate.as_view(), name='questions_update'),
+    path('polls/<int:pk>/questions/', QuestionList.as_view(), name="questions-list"),
+    path('polls/<int:pk>/questions/create', QuestionCreate.as_view(), name="questions-create"),
+    path('polls/<int:pk>/questions/<int:q_pk>/', QuestionDetail.as_view(), name='questions-detail'),
+    path('questions/<int:pk>/update', QuestionUpdate.as_view(), name='questions-update'),
 
-    path('polls/<int:pk>/questions/<int:q_pk>/choices/', ChoiceList.as_view(), name='choices_list'),
-    path('polls/<int:pk>/questions/<int:q_pk>/choices/create', ChoiceCreate.as_view(), name='choices_create'),
-    path('choices/<int:pk>/update', ChoiceUpdate.as_view(), name='choices_update'),
+    path('polls/<int:pk>/questions/<int:q_pk>/choices/', ChoiceList.as_view(), name='choices-list'),
+    path('polls/<int:pk>/questions/<int:q_pk>/choices/create', ChoiceCreate.as_view(), name='choices-create'),
+    path('choices/<int:pk>/update', ChoiceUpdate.as_view(), name='choices-update'),
 
-    path('polls/<int:pk>/questions/<int:q_pk>/answer', CreateAnswer.as_view(), name='create_answer'),
-    path('polls/my-answers', MyAnswers.as_view(), name='my_answers'),
+    path('polls/<int:pk>/questions/<int:q_pk>/answer', CreateAnswer.as_view(), name='create-answer'),
+    path('polls/my-answers', MyAnswers.as_view(), name='my-answers'),
 
-    path('create-id', CreateId.as_view(), name='create_id')
+    path('create-id', CreateId.as_view(), name='create-id')
 ]
