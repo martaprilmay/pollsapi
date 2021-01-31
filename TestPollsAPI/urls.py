aa6e40f29dabee3de1b67fbd52bdc5dce1ddba82
Martin Kroll <mrtnkrll@gmail.com>
2021-01-31 21:55:12 +0300

    Deleted auto-generated API docs which sucked

--- TestPollsAPI/urls.py
+++ TestPollsAPI/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views
 
-from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,5 +9,3 @@ urlpatterns = [
     path('api/v1/base-auth/', include('rest_framework.urls')),
     path('api/v1/token-auth/', views.obtain_auth_token, name='api-token-auth'),
 ]
-
-urlpatterns += doc_urls
