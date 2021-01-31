d7c3f570c6d2b874aaba226b92a25792581117dc
Martin Kroll <mrtnkrll@gmail.com>
2021-01-31 13:07:41 +0300

    Added token auth

--- TestPollsAPI/settings.py
+++ TestPollsAPI/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken',
     'polls',
 ]
 
@@ -102,6 +103,15 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
 
 
 # Internationalization
--- TestPollsAPI/urls.py
+++ TestPollsAPI/urls.py
@@ -1,9 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('polls.urls')),
     path('api/v1/base-auth/', include('rest_framework.urls')),
+    path('api/v1/token-auth/', views.obtain_auth_token, name='api-token-auth'),
 ]
