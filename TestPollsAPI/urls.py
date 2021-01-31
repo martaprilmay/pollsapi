c363c74ffa21c21918eaaacd25c72f36c40e42a2
Martin Kroll <mrtnkrll@gmail.com>
2021-01-31 21:38:12 +0300

    Added autogenerated API docs

--- TestPollsAPI/urls.py
+++ TestPollsAPI/urls.py
@@ -1,7 +1,8 @@
-
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views
+
+from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,3 +10,5 @@ urlpatterns = [
     path('api/v1/base-auth/', include('rest_framework.urls')),
     path('api/v1/token-auth/', views.obtain_auth_token, name='api-token-auth'),
 ]
+
+urlpatterns += doc_urls
